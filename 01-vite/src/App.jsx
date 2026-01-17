b416d0392db50901399b7fcbf395b5e319740640
Suhagshetty <suhagshetty07@gmail.com>
2026-01-17 21:41:22 +0530

    creating react app via vite

---
+++ 01-vite/src/App.jsx
@@ -0,0 +1,11 @@
+import Chai from "./Chai"
+function ViteApp(){
+  return(
+    <>
+    <p>Welcome to React Series</p>
+     <Chai/>
+    </>
+
+  )
+}
+export default ViteApp
---
+++ 01-vite/src/Chai.jsx
@@ -0,0 +1,9 @@
+function Chai(){
+    return(
+        <div>
+          <h2>This is the modern way of creating a React App - VITE</h2>
+          <p>This was created using npx create-vite@latest</p>
+        </div>
+    )
+}
+export default Chai
