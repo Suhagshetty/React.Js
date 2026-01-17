3ff7d2b63cad78af47e53625163cf05b3ddb291a
Suhagshetty <suhagshetty07@gmail.com>
2026-01-17 21:30:09 +0530

    reactapp using the dumb way

---
+++ 00-react/src/App.js
@@ -0,0 +1,10 @@
+function App(){
+  return(
+    <div>
+      <h2>This is the basic way of Creating a React App</h2>
+      <h3>This is the worst possible way of creating a react app</h3>
+      <p>This was created using npx-create-react-app</p>
+    </div>
+  )
+}
+export default App
